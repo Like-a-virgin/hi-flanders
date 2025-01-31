3b98940ca502663a3b10a531d371594f6b5066c8
Claudine <claudine@likeavirgin.be>
2025-01-31 12:19:35 +0100

    new number format

--- modules/membership-payments/controllers/PaymentController.php
+++ modules/membership-payments/controllers/PaymentController.php
@@ -128,8 +128,8 @@ class PaymentController extends Controller
                 "print" => $printPayment,
                 "memberships" => $membershipPayments,
                 "total" => number_format($totalFormatted, 2, '.', ''),
-                "membershipTotal" => number_format($totalMembershipRate->getAmount()), 
-                "printTotal" => number_format($totalPrintRate->getAmount()), 
+                "membershipTotal" => $totalMembershipRate->getAmount(), 
+                "printTotal" => $totalPrintRate->getAmount(), 
             ],
         ]);
 
