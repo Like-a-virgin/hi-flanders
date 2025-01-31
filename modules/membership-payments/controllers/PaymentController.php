0ad3d6e039dfc83c2ff312f8eae7502a7270225f
Claudine <claudine@likeavirgin.be>
2025-01-31 11:26:44 +0100

    try 2

--- modules/membership-payments/controllers/PaymentController.php
+++ modules/membership-payments/controllers/PaymentController.php
@@ -293,7 +293,7 @@ class PaymentController extends Controller
             $mailer = Craft::$app->mailer;
             Craft::$app->getView()->setTemplatesPath(Craft::getAlias('@root/templates'));
 
-            $htmlBody = Craft::$app->getView()->renderTemplate('email/verification/verification-ind-payed', [
+            $htmlBody = Craft::$app->getView()->renderTemplate('email/request/request-print', [
                 'name' => $user->getFieldValue('altFirstName'),
             ]);
 
