748b1cff19adb3dd2799d3123b294f60accb3d1e
Claudine <claudine@likeavirgin.be>
2025-04-08 09:32:07 +0200

    fixed see password

--- src/scripts/see-pass.js
+++ src/scripts/see-pass.js
@@ -1,19 +1,14 @@
 const eye = document.querySelector('.formbox__see-pass-icon');
-console.log('eye' + eye)
 
 if (eye) {
     document.addEventListener('DOMContentLoaded', () => {
         const passwordInput = document.getElementById('password');
-        console.log('passInp' + passwordInput)
         const togglePasswordButton = document.getElementById('togglePassword');
-        console.log('toggleBtn' + togglePasswordButton)
 
         if (!passwordInput || !togglePasswordButton) return; // Prevent errors if elements are missing
 
         togglePasswordButton.addEventListener('click', () => {
-            console.log('clicked')
             const isPasswordVisible = passwordInput.type === 'text';
-            console.log('ispassvis' + isPasswordVisible)
             passwordInput.type = isPasswordVisible ? 'password' : 'text';
 
             // Toggle the eye icon class
